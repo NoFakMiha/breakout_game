624fcfc5736a7d325f91d32bb021bdc7473b260d
NoFakMiha <53381761+NoFakMiha@users.noreply.github.com>
2021-10-17 20:57:59 +0200

    Update break_out.py
    Game board | Player movement added

--- break_out.py
+++ break_out.py
@@ -1,11 +1,16 @@
+import tkinter as tk
 from turtle import Screen
 from blocks import Blocks, BLOCK_WIDTH
+from player import  PLayer
+
+Blocks()
+user = PLayer()
+
 screen = Screen()
+screen.listen()
+screen.onkeypress(user.player_move_right, "Right")
+screen.onkeypress(user.player_move_left, "Left")
 
-screen.setup(width=500, height=500)
-
-for i in range(2):
-    block = Blocks()
 
 
 
