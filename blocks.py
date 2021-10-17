a05b6eae5836327ed199c59e96f04876f184805a
NoFakMiha <53381761+NoFakMiha@users.noreply.github.com>
2021-10-17 20:59:54 +0200

    Update blocks.py
    Add player movement | Game Board

--- blocks.py
+++ blocks.py
@@ -1,20 +1,32 @@
 import turtle
+import random
+from turtle import Screen
 # Blocks settings
 SHAPE = "square"
 BLOCK_WIDTH = 1
 SIZE_LENGTH = 8
-START_X = -240
-START_Y = 240
+COLOR = "#393E46", "#00ADB5", "#EEEEEE", "#F9ED69"
+start_x = -240
+start_y = 240
 
 class Blocks():
     def __init__(self):
+        start_x = -240
+        start_y = 240
 
-        self.block = turtle.Turtle(SHAPE)
-        self.block.hideturtle()
-        self.block.speed(10)
-        self.block.penup()
-        self.block.goto(START_X,START_Y)
-
-        self.block.showturtle()
-
-        self.block.shapesize(stretch_wid=BLOCK_WIDTH, stretch_len=SIZE_LENGTH)
+        for i in range(14):
+            self.block = turtle.Turtle(SHAPE)
+            self.block.penup()
+            self.block.hideturtle()
+            self.block.fillcolor(random.choice(COLOR))
+            self.block.speed(10)
+            self.block.goto(start_x,start_y)
+            self.block.showturtle()
+            self.block.shapesize(stretch_wid=BLOCK_WIDTH, stretch_len=SIZE_LENGTH)
+            start_x = start_x + 150
+            if i == 4 or i == 8:
+                start_x = -240
+                start_y -= 20
+        self.screen = Screen()
+        self.screen.bgcolor("#222831")
+        self.screen.setup(width=500, height=500)
